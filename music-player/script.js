ae94a757f0a9a95dd17efc675b6c85e8e68ba049
iuliancarnaru <iulian.carnaru@gmail.com>
2021-02-25 17:04:58 +0000

    music player

---
+++ music-player/script.js
@@ -0,0 +1,24 @@
+const music = document.querySelector('audio');
+const prevBtn = document.getElementById('prev');
+const playBtn = document.getElementById('play');
+const nextBtn = document.getElementById('next');
+
+// Check if is playing
+let isPlaying = false;
+
+// Play song
+function playSong() {
+  isPlaying = true;
+  playBtn.classList.replace('fa-play', 'fa-pause');
+  playBtn.setAttribute('title', 'Pause');
+  music.play();
+}
+// Pause song
+function pauseSong() {
+  isPlaying = false;
+  playBtn.classList.replace('fa-pause', 'fa-play');
+  playBtn.setAttribute('title', 'Play');
+  music.pause();
+}
+
+playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
