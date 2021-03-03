4e66164822575fe7d4431bd46f6cd93eecad2669
iuliancarnaru <iulian.carnaru@gmail.com>
2021-03-03 22:40:17 +0000

    adding storage logic

--- bookmark-keeper/script.js
+++ bookmark-keeper/script.js
@@ -6,6 +6,8 @@ const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
 const bookmarksContainer = document.getElementById('bookmarks-container');
 
+let bookmarks = [];
+
 // Show modal (focus on first input)
 function showModal() {
   modal.classList.add('show-modal');
@@ -22,3 +24,126 @@ modalClose.addEventListener('click', closeModal);
 window.addEventListener('click', (event) =>
   event.target === modal ? modal.classList.remove('show-modal') : false
 );
+
+// validate form
+function validate(nameValue, urlValue) {
+  const expresion = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+
+  const regex = new RegExp(expresion);
+
+  if (!nameValue || !urlValue) {
+    alert('please submit values for both fields');
+    return false;
+  }
+
+  if (!urlValue.match(regex)) {
+    alert('please provide a valid web address');
+    return false;
+  }
+
+  return true;
+}
+
+// build bookmarks in DOM
+function buildBookmarks() {
+  // remove everyting from bookmarks container
+  bookmarksContainer.textContent = '';
+
+  bookmarks.forEach((bookmark) => {
+    const { name, url } = bookmark;
+    // item
+    const item = document.createElement('div');
+    item.classList.add('item');
+
+    // close icon
+    const closeIcon = document.createElement('i');
+    closeIcon.classList.add('fas', 'fa-times');
+    closeIcon.setAttribute('title', 'Delete Bookmark');
+    closeIcon.setAttribute('onclick', `deleteBookmark('${url}')`);
+
+    // favicon / link container
+    const linkInfo = document.createElement('div');
+    linkInfo.classList.add('name');
+
+    // favicon
+    const favicon = document.createElement('img');
+    favicon.setAttribute(
+      'src',
+      `https://s2.googleusercontent.com/s2/favicons?domain=${url}`
+    );
+    favicon.setAttribute('alt', 'Favicon');
+
+    // link
+    const link = document.createElement('a');
+    link.setAttribute('href', `${url}`);
+    link.setAttribute('target', '_blank');
+    link.setAttribute('rel', 'noopener noreferrer');
+    link.textContent = name;
+
+    // append to bookmarks container
+    linkInfo.append(favicon, link);
+    item.append(closeIcon, linkInfo);
+    bookmarksContainer.appendChild(item);
+  });
+}
+
+// fetch bookmarks
+function fetchBookmarks() {
+  if (localStorage.getItem('bookmarks')) {
+    bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  } else {
+    bookmarks = [
+      {
+        name: 'Iulian Carnaru',
+        url: 'https://iuliancarnaru.uk',
+      },
+    ];
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  }
+  buildBookmarks();
+}
+
+// delete bookmark
+function deleteBookmark(url) {
+  bookmarks.forEach((bookmark, idx) => {
+    if (bookmark.url === url) {
+      bookmarks.splice(idx, 1);
+    }
+  });
+
+  // update bookmarks array in local storage, repopulate dom
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  fetchBookmarks();
+}
+
+// handle data from form
+function storeBookmark(event) {
+  event.preventDefault();
+  const nameValue = websiteNameEl.value;
+  let urlValue = websiteUrlEl.value;
+
+  if (!urlValue.includes('https://') && !urlValue.includes('http://')) {
+    urlValue = `https://${urlValue}`;
+  }
+
+  if (!validate(nameValue, urlValue)) {
+    return false;
+  }
+
+  const bookmark = {
+    name: nameValue,
+    url: urlValue,
+  };
+
+  bookmarks.push(bookmark);
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  fetchBookmarks();
+  bookmarkForm.reset();
+  websiteNameEl.focus();
+}
+
+// event listener
+bookmarkForm.addEventListener('submit', storeBookmark);
+
+// on load
+fetchBookmarks();
