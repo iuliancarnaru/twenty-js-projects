40ca2ff95ce8c8105ea65bcbe4d893031ec58a05
iuliancarnaru <iulian.carnaru@gmail.com>
2021-03-11 13:42:42 +0000

    creating dom elements

--- nasa-apod/script.js
+++ nasa-apod/script.js
@@ -1,3 +1,9 @@
+const resultsNav = document.getElementById('resultsNav');
+const favoritesNav = document.getElementById('favoritesNav');
+const imagesContainer = document.querySelector('.images-container');
+const saveConfirmed = document.querySelector('.save-confirmed');
+const loader = document.querySelector('.loader');
+
 // NASA API
 const count = 10;
 const apiKEY = 'DEMO_KEY';
@@ -5,12 +11,22 @@ const apiURL = `https://api.nasa.gov/planetary/apod?api_key=${apiKEY}&count=${co
 
 let resultsArray = [];
 
+function updateDOM() {
+  resultsArray.forEach((result) => {
+    // card container
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    // link
+  });
+}
+
 // get 10 images from NASA api
 async function getNasaPictures() {
   try {
     const response = await fetch(apiURL);
     resultsArray = await response.json();
-    console.log(resultsArray);
+    updateDOM();
   } catch (error) {
     console.error(error);
   }
